34207edcc7b98ce54b707769b9804a34d6c21ef5
FabrizioCmp <fabriziocampari01@gmail.com>
2023-04-20 19:03:43 +0200

    adds formatting with null-ls

--- lua/fabri/lsp/mason.lua
+++ lua/fabri/lsp/mason.lua
@@ -8,6 +8,12 @@ if not mason_lspconfig_status then
   return
 end
 
+local mason_null_ls_status, mnl = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+		print("mason-null-ls problem")
+		return
+end
+
 
 
 
@@ -24,4 +30,11 @@ mason_lspconfig.setup({
 		}
 })
 
+mnl.setup({
+		ensure_installed = {
+				"prettier",
+				"stylua",
+				"eslint_d"
+		}
 
+})
